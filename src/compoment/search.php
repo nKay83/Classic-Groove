263b52bcbc44a9d53293bbe03bdbcc9f5695b0bf
BuiBao303 <hongbao2003@gmail.com>
2023-03-03 02:04:21 +0700

    fix search bar on desktop

--- src/compoment/search.php
+++ src/compoment/search.php
@@ -1,17 +1,24 @@
-<div class="search">
-  <i class="fa-solid fa-magnifying-glass"></i>
-  <input type="text" id="search-btn" placeholder="What do you want to listen or buy?">
+<div class="search-container">
+  <div class="search">
+    <i class="fa-solid fa-magnifying-glass"></i>
+    <input type="text" id="search-btn" placeholder="What do you want to listen or buy?">
+  </div>
 </div>
-<div class="drop-menu">
-  <i class="fa-sharp fa-regular fa-bars-filter"></i>
-  <select name="" id="drop-menu-btn">
-    <option value="">All kind of music you know</option>
-    <option value="">b</option>
-    <option value="">c</option>
-  </select>
+<div class="drop-menu-container">
+
+  <div class="drop-menu">
+    <i class="fa-sharp fa-regular fa-bars-filter"></i>
+    <select name="" id="drop-menu-btn">
+      <option value="">All kind of music you know</option>
+      <option value="">b</option>
+      <option value="">c</option>
+    </select>
+  </div>
 </div>
-<div class="price">
-  <i class="fa-regular fa-coin"></i>
-  <i class="fa-regular fa-coins"></i>
-  <input type="range" min="0" max="500" value="0" name="" id="price-ptn">
+<div class="price-container">
+  <div class="price">
+    <i class="fa-regular fa-coin"></i>
+    <input type="range" min="0" max="500" value="0" name="" id="price-ptn">
+    <i class="fa-regular fa-coins"></i>
+  </div>
 </div>
