0868ff6a12aecd8514d3e847ead746b2a346f770
BuiBao303 <hongbao2003@gmail.com>
2023-03-03 03:02:53 +0700

    complete home

---
+++ src/compoment/header.php
@@ -0,0 +1,50 @@
+<div class="background">
+  <div class="logo-placeholder">
+    <img src="../assets/img/Logo.png" alt="">
+  </div>
+  <div class="top-menu">
+    <div class="tab-title active">
+      <div class="tab-icon">
+        <i class="fa-solid fa-house "></i>
+      </div>
+      <div class="tab-info">
+        Home
+      </div>
+    </div>
+    <div class="tab-title">
+      <div class="tab-icon">
+        <i class="fa-regular fa-heart "></i>
+      </div>
+      <div class="tab-info">
+        Favorite
+      </div>
+    </div>
+    <div class="tab-title">
+      <div class="tab-icon">
+        <i class="fa-brands fa-opencart "></i>
+      </div>
+      <div class="tab-info">
+        My cart
+      </div>
+    </div>
+    <div class="tab-title">
+      <div class="tab-icon">
+        <i class="fa-solid fa-user "></i>
+      </div>
+      <div class="tab-info">
+        My account
+      </div>
+    </div>
+  </div>
+  <div class="button-placeholder">
+    <div class="left-button-login">
+      <input type="button" value="Log in">
+    </div>
+    <div class="right-button-register">
+      <input type="button" value="Sign up">
+    </div>
+  </div>
+  <div class="footer-placeholder">
+    <p>About us</p>
+  </div>
+</div>
--- src/compoment/home.php
+++ src/compoment/home.php
@@ -10,4 +10,47 @@
       <div class="sub-right"></div>
     </div>
   </div>
+  <h1>Features</h1>
+  <div class="grid-container">
+    <div class="grid-item">
+      <div class="img-container">
+        <img src="https://via.placeholder.com/250x250" alt="Placeholder">
+        <div class="img-overlay">
+          <div>
+            <i class="fa-solid fa-play"></i>
+          </div>
+        </div>
+      </div>
+      <p>Headline</p>
+      <p class="gray">Captions</p>
+    </div>
+    <div class="grid-item">
+      <div class="img-container">
+        <img src="https://via.placeholder.com/250x250" alt="Placeholder">
+      </div>
+      <p>Headline</p>
+      <p class="gray">Captions</p>
+    </div>
+    <div class="grid-item">
+      <div class="img-container">
+        <img src="https://via.placeholder.com/250x250" alt="Placeholder">
+      </div>
+      <p>Headline</p>
+      <p class="gray">Captions</p>
+    </div>
+    <div class="grid-item">
+      <div class="img-container">
+        <img src="https://via.placeholder.com/250x250" alt="Placeholder">
+      </div>
+      <p>Headline</p>
+      <p class="gray">Captions</p>
+    </div>
+    <div class="grid-item">
+      <div class="img-container">
+        <img src="https://via.placeholder.com/250x250" alt="Placeholder">
+      </div>
+      <p>Headline</p>
+      <p class="gray">Captions</p>
+    </div>
+  </div>
 </div>
