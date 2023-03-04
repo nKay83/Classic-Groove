48f43018e3866f6f95910b2823397eed252583e0
PhuQuangKoiNe <tnpquang@gmail.com>
2023-03-04 20:55:38 +0700

    Fix my cart

--- src/compoment/myCart.php
+++ src/compoment/myCart.php
@@ -1,9 +1,43 @@
 <div id="mycart">
     <div class="left-placeholder">
         <div class="product-placeholder">
-            <div class="product-img-placeholder"></div>
+            <div class="product-img-placeholder">
+                <img src="../assets/img/product.png" alt="">
+            </div>
             <div class="product-info-placeholder">
-                
+                <div class="album-name">GRRR Live! (Exclusive Red Limited Edition)</div>
+                <div class="album-artist">The Rolling Stones</div>
+                <div class="product-kind">Vinyl disc</div>
+                <div class="QPT">
+                    <!-- <div class="quantity">Quantity</div>
+                    <div class="price">Price</div>
+                    <div class="total">Total</div>
+                </div>
+                <div class="QPT-info">
+                    <div class="quantity-placeholder">
+                        <div class="quantity-info">-</div>
+                        <div class="quantity-info" id="number">1</div>
+                        <div class="quantity-info">+</div>
+                    </div>
+                    <div class="price-placeholder">$125.00</div>
+                    <div class="price-placeholder">$125.00</div> -->
+                    <div class="QPT-info">Quantity</div>
+                    <div class="QPT-info">Price</div>
+                    <div class="QPT-info">Total</div>
+                    <div class="QPT-info">
+                        <div class="quantity-placeholder">
+                            <div class="quantity-info"><i class="fa-solid fa-minus fa-xs"></i></div>
+                            <div class="quantity-info">1</div>
+                            <div class="quantity-info"><i class="fa-solid fa-plus-large fa-xs"></i></div>
+                        </div>
+                    </div>
+                    <div class="QPT-info">$125.00</div>
+                    <div class="QPT-info">$125.00</div>
+                </div>
+            </div>
+            <div class="button-placeholder">
+                <div class="erase-button"><i class="fa-solid fa-xmark fa-lg"></i></div>
+                <div class="check-button"><input type="checkbox"></div>
             </div>
         </div>
     </div>
