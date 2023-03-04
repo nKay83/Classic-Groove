3f204597bacf39a665fb05a70e1f4ddf67046574
BuiBao303 <hongbao2003@gmail.com>
2023-03-04 23:23:38 +0700

    fix input in mycart

--- src/compoment/myCart.php
+++ src/compoment/myCart.php
@@ -15,7 +15,7 @@
                     <div class="QPT-info">
                         <div class="quantity-placeholder">
                             <div class="quantity-info"><i class="fa-solid fa-minus fa-xs"></i></div>
-                            <div class="quantity-info">1</div>
+                            <input type="text" class="quantity-info" value="1">
                             <div class="quantity-info"><i class="fa-solid fa-plus-large fa-xs"></i></div>
                         </div>
                     </div>
@@ -25,63 +25,9 @@
             </div>
             <div class="button-placeholder">
                 <div class="erase-button"><i class="fa-solid fa-xmark fa-lg"></i></div>
-                <div class="check-button"></div>
-            </div>
-        </div>
-        <div class="product-placeholder">
-            <div class="product-img-placeholder">
-                <img src="../assets/img/product.png" alt="">
-            </div>
-            <div class="product-info-placeholder">
-                <div class="album-name">GRRR Live! (Exclusive Red Limited Edition)</div>
-                <div class="album-artist">The Rolling Stones</div>
-                <div class="product-kind">Vinyl disc</div>
-                <div class="QPT">
-                    <div class="QPT-info">Quantity</div>
-                    <div class="QPT-info">Price</div>
-                    <div class="QPT-info">Total</div>
-                    <div class="QPT-info">
-                        <div class="quantity-placeholder">
-                            <div class="quantity-info"><i class="fa-solid fa-minus fa-xs"></i></div>
-                            <div class="quantity-info">1</div>
-                            <div class="quantity-info"><i class="fa-solid fa-plus-large fa-xs"></i></div>
-                        </div>
-                    </div>
-                    <div class="QPT-info">$125.00</div>
-                    <div class="QPT-info">$125.00</div>
+                <div class="check-button">
+                    <input type="checkbox">
                 </div>
-            </div>
-            <div class="button-placeholder">
-                <div class="erase-button"><i class="fa-solid fa-xmark fa-lg"></i></div>
-                <div class="check-button"></div>
-            </div>
-        </div>
-        <div class="product-placeholder">
-            <div class="product-img-placeholder">
-                <img src="../assets/img/product.png" alt="">
-            </div>
-            <div class="product-info-placeholder">
-                <div class="album-name">GRRR Live! (Exclusive Red Limited Edition)</div>
-                <div class="album-artist">The Rolling Stones</div>
-                <div class="product-kind">Vinyl disc</div>
-                <div class="QPT">
-                    <div class="QPT-info">Quantity</div>
-                    <div class="QPT-info">Price</div>
-                    <div class="QPT-info">Total</div>
-                    <div class="QPT-info">
-                        <div class="quantity-placeholder">
-                            <div class="quantity-info"><i class="fa-solid fa-minus fa-xs"></i></div>
-                            <div class="quantity-info">1</div>
-                            <div class="quantity-info"><i class="fa-solid fa-plus-large fa-xs"></i></div>
-                        </div>
-                    </div>
-                    <div class="QPT-info">$125.00</div>
-                    <div class="QPT-info">$125.00</div>
-                </div>
-            </div>
-            <div class="button-placeholder">
-                <div class="erase-button"><i class="fa-solid fa-xmark fa-lg"></i></div>
-                <div class="check-button"></div>
             </div>
         </div>
     </div>
