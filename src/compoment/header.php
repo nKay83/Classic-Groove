fc2498c719ec7c3deb77bf0d3168c6a9fe9a86bb
BuiBao303 <hongbao2003@gmail.com>
2023-03-08 21:38:48 +0700

    fix header

--- src/compoment/header.php
+++ src/compoment/header.php
@@ -1,74 +1,81 @@
 <div class="background">
-  <div class="logo-placeholder">
-    <img src="../assets/img/Logo.png" alt="">
-  </div>
-  <div class="top-menu">
-    <div class="tab-title active">
-      <div class="tab-icon">
-        <i class="fa-solid fa-house "></i>
-      </div>
-      <div class="tab-info">
-        <a href="index.php?page=home">Home</a>
-      </div>
+  <div class="top">
+    <div class="logo-placeholder">
+      <img src="../assets/img/Logo.png" alt="">
     </div>
-    <div class="tab-title">
-      <div class="tab-icon">
-        <i class="fa-solid fa-heart"></i>
+    <div class="top-menu">
+      <div class="tab-title active">
+        <div class="tab-icon">
+          <i class="fa-solid fa-house "></i>
+        </div>
+        <div class="tab-info">
+          <a href="index.php?page=home">Home</a>
+        </div>
       </div>
-      <div class="tab-info">
-        <a href="index.php?page=favorites">Favorites</a>
+      <div class="tab-title">
+        <div class="tab-icon">
+          <i class="fa-solid fa-heart"></i>
+        </div>
+        <div class="tab-info">
+          <a href="index.php?page=favorites">Favorites</a>
+        </div>
+        <div class="tab-notice">
+          <div class="tab-notice-headline">Access Your Favorites</div>
+          <div class="tab-notice-info">Log in to see saved songs, albums, artists in Favorties</div>
+          <div class="button-placeholder">
+            <input type="button" value="Not now">
+            <input type="button" value="Log in" style="color:">
+          </div>
+        </div>
       </div>
-      <div class="tab-notice">
-        <div class="tab-notice-headline">Access Your Favorites</div>
-        <div class="tab-notice-info">Log in to see saved songs, albums, artists in Favorties</div>
-        <div class="button-placeholder">
-          <input type="button" value="Not now">
-          <input type="button" value="Log in" style="color:">
+      <div class="tab-title">
+        <div class="tab-icon">
+          <i class="fa-brands fa-opencart "></i>
+        </div>
+        <div class="tab-info">
+          <a href="index.php?page=myCart">My cart</a>
+        </div>
+        <div class="tab-notice">
+          <div class="tab-notice-headline">Access Your Cart</div>
+          <div class="tab-notice-info">Log in to check, add, remove products in My Cart</div>
+          <div class="button-placeholder">
+            <input type="button" value="Not now">
+            <input type="button" value="Log in" style="color:">
+          </div>
+        </div>
+      </div>
+      <div class="tab-title">
+        <div class="tab-icon">
+          <i class="fa-solid fa-user "></i>
+        </div>
+        <div class="tab-info">
+          <a href="index.php?page=myAccount">My account</a>
+        </div>
+        <div class="tab-notice">
+          <div class="tab-notice-headline">Access Your Account</div>
+          <div class="tab-notice-info">Log in to see profile, purchase history in My Account </div>
+          <div class="button-placeholder">
+            <input type="button" value="Not now">
+            <input type="button" value="Log in" style="color:">
+          </div>
         </div>
       </div>
     </div>
-    <div class="tab-title">
-      <div class="tab-icon">
-        <i class="fa-brands fa-opencart "></i>
+  </div>
+  <div class="bottom">
+    <div class="button-placeholder">
+      <div class="left-button-login">
+        <input type="button" value="Log in">
       </div>
-      <div class="tab-info">
-        <a href="index.php?page=myCart">My cart</a>
-      </div>
-      <div class="tab-notice">
-        <div class="tab-notice-headline">Access Your Cart</div>
-        <div class="tab-notice-info">Log in to check, add, remove products in My Cart</div>
-        <div class="button-placeholder">
-          <input type="button" value="Not now">
-          <input type="button" value="Log in" style="color:">
-        </div>
+      <div class="right-button-register">
+        <input type="button" value="Sign up">
       </div>
     </div>
-    <div class="tab-title">
-      <div class="tab-icon">
-        <i class="fa-solid fa-user "></i>
-      </div>
-      <div class="tab-info">
-        <a href="index.php?page=myAccount">My account</a>
-      </div>
-      <div class="tab-notice">
-        <div class="tab-notice-headline">Access Your Account</div>
-        <div class="tab-notice-info">Log in to see profile, purchase history in My Account </div>
-        <div class="button-placeholder">
-          <input type="button" value="Not now">
-          <input type="button" value="Log in" style="color:">
-        </div>
-      </div>
+    <div class="footer-placeholder">
+      <p>About us</p>
     </div>
   </div>
-  <div class="button-placeholder">
-    <div class="left-button-login">
-      <input type="button" value="Log in">
-    </div>
-    <div class="right-button-register">
-      <input type="button" value="Sign up">
-    </div>
-  </div>
-  <div class="footer-placeholder">
-    <p>About us</p>
-  </div>
+
+
+
 </div>
