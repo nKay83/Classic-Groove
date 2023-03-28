b040f7910d18018a62f28d7b709f1513c686010d
PhuQuangKoiNe <tnpquang@gmail.com>
2023-03-28 12:25:53 +0700

    Add reponsive for admin pages

--- pages/admin/accountManager.php
+++ pages/admin/accountManager.php
@@ -6,11 +6,11 @@
             <div class="info-placeholder">Edit</div>
         </div>
         <div class="save-button">
-            <div class="icon-placeholder"><i class="fa-solid fa-plus"></i></div>
+            <div class="icon-placeholder"><i class="fa-solid fa-user-plus"></i></div>
             <div class="info-placeholder">New</div>
         </div>
         <div class="save-button">
-            <div class="icon-placeholder"><i class="fa-duotone fa-check"></i></div>
+            <div class="icon-placeholder"><i class="fa-solid fa-folder-arrow-down"></i></div>
             <div class="info-placeholder">Save</div>
         </div>
         <div class="delete-button">
@@ -101,4 +101,122 @@
             </div>
         </div>
     </div>
+    <!-- <div class="modal-placeholder">
+        <div class="modal-box">
+            <div class="modal-header">
+                <h1><i class="fa-regular fa-pen-to-square"></i> Edit profile</h1>
+            </div>
+            <div class="modal-info">
+                <div class="modal-item">
+                    <div class="item-header">Profile id</div>
+                    <div class="item-input"><input type="text" value="CUS001"></div>
+                </div>
+                <div class="modal-item">
+                    <div class="item-header">Profile name</div>
+                    <div class="item-input"><input type="text" value="Bùi Hồng Bảo"></div>
+                </div>
+                <div class="modal-item">
+                    <div class="item-header">Email</div>
+                    <div class="item-input"><input type="text" value="buibuibaobao@gmail.com"></div>
+                </div>
+                <div class="modal-item">
+                    <div class="item-header">Phone number</div>
+                    <div class="item-input"><input type="text" value="0900000000"></div>
+                </div>
+                <div class="modal-item">
+                    <div class="item-header">Address</div>
+                    <div class="item-input"><input type="text" value="Ho Chi Minh City"></div>
+                </div>
+                <div class="modal-item">
+                    <div class="item-header">Role</div>
+                    <div class="item-input"><select name="" id="">
+                            <option value="CUS">Customer</option>
+                            <option value="EM">Employee</option>
+                            <option value="AD">Admin</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="modal-item">
+                    <div class="item-header">Account name</div>
+                    <div class="item-input"><input type="text" value="baohongbui313"></div>
+                </div>
+                <div class="modal-item">
+                    <div class="item-header">Password</div>
+                    <div class="item-input"><input type="text" value="baobaobuibui"></div>
+                </div>
+            </div>
+            <div class="modal-button">
+                <div class="button-layout"></div>
+                <div class="button-layout">
+                    <div class="edit-button">
+                        <div class="icon-placeholder"><i class="fa-solid fa-folder-arrow-down"></i></div>
+                        <div class="info-placeholder">Save</div>
+                    </div>
+                    <div class="back-button">
+                        <div class="icon-placeholder"><i class="fa-solid fa-xmark"></i></div>
+                        <div class="info-placeholder">Cancel</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
+    <!-- <div class="modal-placeholder">
+        <div class="modal-box">
+            <div class="modal-header">
+                <h1><i class="fa-solid fa-user-plus"></i> New profile</h1>
+            </div>
+            <div class="modal-info">
+                <div class="modal-item">
+                    <div class="item-header">Profile id</div>
+                    <div class="item-input"><input type="text" value="CUS001"></div>
+                </div>
+                <div class="modal-item">
+                    <div class="item-header">Profile name</div>
+                    <div class="item-input"><input type="text" value="Bùi Hồng Bảo"></div>
+                </div>
+                <div class="modal-item">
+                    <div class="item-header">Email</div>
+                    <div class="item-input"><input type="text" value="buibuibaobao@gmail.com"></div>
+                </div>
+                <div class="modal-item">
+                    <div class="item-header">Phone number</div>
+                    <div class="item-input"><input type="text" value="0900000000"></div>
+                </div>
+                <div class="modal-item">
+                    <div class="item-header">Address</div>
+                    <div class="item-input"><input type="text" value="Ho Chi Minh City"></div>
+                </div>
+                <div class="modal-item">
+                    <div class="item-header">Role</div>
+                    <div class="item-input"><select name="" id="">
+                            <option value="CUS">Customer</option>
+                            <option value="EM">Employee</option>
+                            <option value="AD">Admin</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="modal-item">
+                    <div class="item-header">Account name</div>
+                    <div class="item-input"><input type="text" value="baohongbui313"></div>
+                </div>
+                <div class="modal-item">
+                    <div class="item-header">Password</div>
+                    <div class="item-input"><input type="text" value="baobaobuibui"></div>
+                </div>
+            </div>
+            <div class="modal-button">
+                <div class="button-layout"></div>
+                <div class="button-layout">
+                    <div class="edit-button">
+                        <div class="icon-placeholder"><i class="fa-solid fa-user-plus"></i></div>
+                        <div class="info-placeholder">Add</div>
+                    </div>
+                    <div class="back-button">
+                        <div class="icon-placeholder"><i class="fa-solid fa-xmark"></i></div>
+                        <div class="info-placeholder">Cancel</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div> -->
 </div>
