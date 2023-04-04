191e1d4ed9f2041788b212ac540bf2a8e74581cd
BuiBao303 <hongbao2003@gmail.com>
2023-04-04 22:42:16 +0700

    load cart

--- views/pages/user/myCart.php
+++ views/pages/user/myCart.php
@@ -1,35 +1,56 @@
+<?php
+require("../../../util/dataProvider.php");
+$dp = new DataProvider();
+session_start();
+$userID = $_SESSION['userID'];
+$sql = "SELECT giohang.soLuong as ghsl, album.*, theloai.* FROM giohang join album on giohang.maAlbum = album.maAlbum
+        join theloai on album.theloai = theloai.maLoai where maKhachHang = '" . $userID . "'";
+$result = $dp->excuteQuery($sql);
+$album = array();
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        array_push($album, $row);
+    }
+}
+?>
 <div id="mycart">
     <div class="left-placeholder">
-        <div class="product-placeholder">
-            <div class="product-img-placeholder">
-                <img src="views/assets/img/product.png" alt="">
-            </div>
-            <div class="product-info-placeholder">
-                <div class="album-name">GRRR Live! (Exclusive Red Limited Edition)</div>
-                <div class="album-artist">The Rolling Stones</div>
-                <div class="product-kind">Vinyl disc</div>
-                <div class="QPT">
-                    <div class="QPT-info">Quantity</div>
-                    <div class="QPT-info">Price</div>
-                    <div class="QPT-info">Total</div>
-                    <div class="QPT-info">
-                        <div class="quantity-placeholder">
-                            <div class="quantity-info"><i class="fa-solid fa-minus fa-xs"></i></div>
-                            <input type="text" class="quantity-info" value="1">
-                            <div class="quantity-info"><i class="fa-solid fa-plus-large fa-xs"></i></div>
+        <?php
+        foreach ($album as $al) {
+            echo '
+            <div class="product-placeholder">
+                <div class="product-img-placeholder">
+                    <img src="data/imgAlbum/' . $al['hinh'] . '.jpg" alt="poster">
+                </div>
+                <div class="product-info-placeholder">
+                    <div class="album-name">' . $al['tenAlbum'] . '</div>
+                    <div class="album-artist">' . $al['tacGia'] . '</div>
+                    <div class="product-kind">' . $al['tenLoai'] . '</div>
+                    <div class="QPT">
+                        <div class="QPT-info">Quantity</div>
+                        <div class="QPT-info">Price</div>
+                        <div class="QPT-info">Total</div>
+                        <div class="QPT-info">
+                            <div class="quantity-placeholder">
+                                <div class="quantity-info"><i class="fa-solid fa-minus fa-xs"></i></div>
+                                <input type="text" class="quantity-info" value="' . $al['ghsl'] . '">
+                                <div class="quantity-info"><i class="fa-solid fa-plus-large fa-xs"></i></div>
+                            </div>
                         </div>
+                        <div class="QPT-info">$' . number_format((float) $al["gia"], 2, '.', '') . '</div>
+                        <div class="QPT-info">$' . number_format((float) ($al['gia'] * $al['soLuong']), 2, '.', '') . '</div>
                     </div>
-                    <div class="QPT-info">$125.00</div>
-                    <div class="QPT-info">$125.00</div>
+                </div>
+                <div class="button-placeholder">
+                    <div class="erase-button"><i class="fa-solid fa-xmark fa-lg"></i></div>
+                    <div class="check-button">
+                        <input type="checkbox">
+                    </div>
                 </div>
             </div>
-            <div class="button-placeholder">
-                <div class="erase-button"><i class="fa-solid fa-xmark fa-lg"></i></div>
-                <div class="check-button">
-                    <input type="checkbox">
-                </div>
-            </div>
-        </div>
+        ';
+        }
+        ?>
     </div>
     <div class="right-placeholder">
         <div class="totalprice-placeholder">
