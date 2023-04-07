0d2f64590e4ca99a2a1863a21f0a3535eaeef2c0
haiduong0916 <haiduong0916@gmail.com>
2023-04-07 22:12:43 +0700

    add login register eyeslash

--- views/js/login.js
+++ views/js/login.js
@@ -1,12 +1,33 @@
-
-
 function eyePassword() {
   let password = document.getElementById("password-field");
+  let eyeicon = document.getElementById("eyeicon");
   if (password.type == "password") {
     password.type = "text";
     eyeicon.className = "fa-thin fa-eye";
   } else {
     password.type = "password";
     eyeicon.className = "fa-thin fa-eye-slash";
+  }
+};
+function eyePassword2() {
+  let password = document.getElementById("password-field2");
+  let eyeicon2 = document.getElementById("eyeicon2");
+  if (password.type == "password") {
+    password.type = "text";
+    eyeicon2.className = "fa-thin fa-eye";
+  } else {
+    password.type = "password";
+    eyeicon2.className = "fa-thin fa-eye-slash";
+  }
+};
+function eyePassword3() {
+  let password = document.getElementById("password-field3");
+  let eyeicon3 = document.getElementById("eyeicon3");
+  if (password.type == "password") {
+    password.type = "text";
+    eyeicon3.className = "fa-thin fa-eye";
+  } else {
+    password.type = "password";
+    eyeicon3.className = "fa-thin fa-eye-slash";
   }
 };
