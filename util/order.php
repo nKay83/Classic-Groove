61c48115f0571eb5806cf7f6ff2ebe73cc1f53d2
BuiBao303 <hongbao2003@gmail.com>
2023-04-08 10:02:18 +0700

    logic order

--- util/order.php
+++ util/order.php
@@ -9,11 +9,22 @@ switch ($_SERVER["REQUEST_METHOD"]) {
         $total = $_POST['total'];
         $address = $_POST['address'];
         $userID = $_SESSION['userID'];
-        $sql = "INSERT INTO hoadon
-        VALUES (''," . $total . ",'" . (new Datetime())->format('Y-m-d') . "',
+        $albums = json_decode($_POST['albums']);
+        $hoaDonID = $dp->getNewHoaDonId();
+        $sql1 = "INSERT INTO hoadon
+        VALUES (" . $hoaDonID . "," . $total . ",'" . (new Datetime())->format('Y-m-d') . "',
         'Đang xử lý','" . $userID . "',null,'" . $address . "')";
-        $result = $dp->excuteQuery($sql);
-        if ($result) {
+        $result1 = $dp->excuteQuery($sql1);
+        $error = false;
+        foreach ($albums as $album) {
+          $sql = "INSERT INTO chitiethoadon
+                  VALUES (" . $album->{"albumID"} . "," . $hoaDonID . "," . $album->{"quantity"} . ")";
+          $result = $dp->excuteQuery($sql);
+          if (!$result) {
+            $error = true;
+          }
+        }
+        if ($result1 && !$error) {
           echo "Success";
         } else {
           echo "Error";
