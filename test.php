09e88d6d70f47cab9ff79af83eaa6df025674eb1
BuiBao3103 <hongbao2003@gmail.com>
2023-04-10 05:57:39 +0700

    add folder

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require("util/dataProvider.php");
-$dp = new DataProvider();
-$username = "Bao";
-$name="bao";
-$phone ="0908141453";
-$sql2 = "INSERT INTO nguoidung
- VALUES ('" . $username . "','" . $name . "','" . $phone . "', null, null, 'Hoạt động', 'KH')";
-$result2 = $dp->excuteQuery($sql2);
