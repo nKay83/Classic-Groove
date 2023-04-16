e5e59bb54ce8a83411c3a41786c5edca13d5a6fb
BuiBao3103 <hongbao2003@gmail.com>
2023-04-16 16:33:07 +0700

    load edit album

--- views/js/modalBox.js
+++ views/js/modalBox.js
@@ -1,111 +1,134 @@
 // Album page
-function openEditalbum() {
-  var editAlbum = document.querySelector("#edit-album");
+let selectAlbum = null;
+let infoAlbum = null;
+let songs = null;
+const openEditalbum = async () => {
+  console.log(infoAlbum);
+  console.log(songs);
+  let editAlbum = document.querySelector("#edit-album");
   editAlbum.style.display = "block";
-}
+  const inputAlbumID = document.querySelector("#edit-album .albumID");
+  const inputAlbumName = document.querySelector("#edit-album .albumName");
+  const inputAlbumKind = document.querySelector("#edit-album .albumKind");
+  const inputAlbumArtist = document.querySelector("#edit-album .albumArtist");
+  const inputAlbumQuanitity = document.querySelector(
+    "#edit-album .albumQuanitity"
+  );
+  const inputAlbumPrice = document.querySelector("#edit-album .albumPrice");
+  const inputAlbumImg = document.querySelector("#edit-album .albumImg");
+  const inputAlbumDescribe = document.querySelector(
+    "#edit-album .albumDescribe"
+  );
+  inputAlbumID.value = infoAlbum.maAlbum;
+  inputAlbumName.value = infoAlbum.tenAlbum;
+  inputAlbumKind.value = infoAlbum.theLoai;
+  inputAlbumArtist.value = infoAlbum.tacGia;
+  inputAlbumQuanitity.value = infoAlbum.soLuong;
+  inputAlbumPrice.value = infoAlbum.gia;
+  inputAlbumImg.value = infoAlbum.hinh;
+  inputAlbumDescribe.value = infoAlbum.moTa;
+};
 const openDetailalbum = async (albumID) => {
-  let infoAlbum = JSON.parse(await getInfoAlbum(albumID));
-  let songs = JSON.parse(await getSongs(albumID));
-  var detailAlbum = document.querySelector("#detail-album");
+  selectAlbum = albumID;
+  infoAlbum = JSON.parse(await getInfoAlbum(selectAlbum));
+  songs = JSON.parse(await getSongs(selectAlbum));
+  let detailAlbum = document.querySelector("#detail-album");
   detailAlbum.style.display = "block";
   const inputAlbumID = document.querySelector("#detail-album .albumID");
   const inputAlbumName = document.querySelector("#detail-album .albumName");
   const inputAlbumKind = document.querySelector("#detail-album .albumKind");
-  const inputAlbumArtist = document.querySelector(
-    "#detail-album .albumArtist"
-  );
-  const inputAlbumQuantity = document.querySelector(
-    "#detail-album .albumQuantity"
+  const inputAlbumArtist = document.querySelector("#detail-album .albumArtist");
+  const inputAlbumQuanitity = document.querySelector(
+    "#detail-album .albumQuanitity"
   );
   const inputAlbumPrice = document.querySelector("#detail-album .albumPrice");
   const inputAlbumImg = document.querySelector("#detail-album .albumImg");
   const inputAlbumDescribe = document.querySelector(
     "#detail-album .albumDescribe"
   );
-
   inputAlbumID.value = infoAlbum.maAlbum;
   inputAlbumName.value = infoAlbum.tenAlbum;
   inputAlbumKind.value = infoAlbum.theLoai;
   inputAlbumArtist.value = infoAlbum.tacGia;
-  inputAlbumQuantity.value = infoAlbum.soLuong;
+  inputAlbumQuanitity.value = infoAlbum.soLuong;
   inputAlbumPrice.value = infoAlbum.gia;
   inputAlbumImg.value = infoAlbum.hinh;
   inputAlbumDescribe.value = infoAlbum.moTa;
 };
 function openNewalbum() {
-  var newAlbum = document.querySelector("#new-album");
+  let newAlbum = document.querySelector("#new-album");
   newAlbum.style.display = "block";
 }
 function openRestockalbum() {
-  var restockAlbum = document.querySelector("#restock-album");
+  let restockAlbum = document.querySelector("#restock-album");
   restockAlbum.style.display = "block";
 }
 
 function closeEditalbum() {
-  var editAlbum = document.querySelector("#edit-album");
+  let editAlbum = document.querySelector("#edit-album");
   editAlbum.style.display = "none";
 }
 
 function closeDetailalbum() {
-  var detailalbum = document.querySelector("#detail-album");
+  let detailalbum = document.querySelector("#detail-album");
   detailalbum.style.display = "none";
 }
 function closeNewalbum() {
-  var newAlbum = document.querySelector("#new-album");
+  let newAlbum = document.querySelector("#new-album");
   newAlbum.style.display = "none";
 }
 function closeRestockalbum() {
-  var restockAlbum = document.querySelector("#restock-album");
+  let restockAlbum = document.querySelector("#restock-album");
   restockAlbum.style.display = "none";
 }
 
 // Order page
 
 function openEditorder() {
-  var editOrder = document.querySelector("#edit-order");
+  let editOrder = document.querySelector("#edit-order");
   editOrder.style.display = "block";
 }
 
 function openDetailorder() {
-  var detailOrder = document.querySelector("#detail-order");
+  let detailOrder = document.querySelector("#detail-order");
   detailOrder.style.display = "block";
 }
 
 function closeEditorder() {
-  var editOrder = document.querySelector("#edit-order");
+  let editOrder = document.querySelector("#edit-order");
   editOrder.style.display = "none";
 }
 
 function closeDetailorder() {
-  var detailOrder = document.querySelector("#detail-order");
+  let detailOrder = document.querySelector("#detail-order");
   detailOrder.style.display = "none";
 }
 
 // Account page
 
 function openEditAccount() {
-  var editAccount = document.querySelector("#edit-account");
+  let editAccount = document.querySelector("#edit-account");
   editAccount.style.display = "block";
 }
 function openDetailAccount() {
-  var detailAccount = document.querySelector("#detail-account");
+  let detailAccount = document.querySelector("#detail-account");
   detailAccount.style.display = "block";
 }
 function openNewAccount() {
-  var newAccount = document.querySelector("#new-account");
+  let newAccount = document.querySelector("#new-account");
   newAccount.style.display = "block";
 }
 
 function closeEditAccount() {
-  var editAccount = document.querySelector("#edit-account");
+  let editAccount = document.querySelector("#edit-account");
   editAccount.style.display = "none";
 }
 function closeDetailAccount() {
-  var detailAccount = document.querySelector("#detail-account");
+  let detailAccount = document.querySelector("#detail-account");
   detailAccount.style.display = "none";
 }
 function closeNewAccount() {
-  var newAccount = document.querySelector("#new-account");
+  let newAccount = document.querySelector("#new-account");
   newAccount.style.display = "none";
 }
 
--- views/pages/admin/productManager.php
+++ views/pages/admin/productManager.php
@@ -75,15 +75,15 @@ $kinds = getKind();
         <div class="modal-info">
           <div class="modal-item">
             <div class="item-header">Album id</div>
-            <div class="item-input"><input type="text" class="albumID"></div>
+            <div class="item-input"><input type="text" class="albumID" disabled></div>
           </div>
           <div class="modal-item">
             <div class="item-header">Album name</div>
-            <div class="item-input"><input type="text" class="albumName"></div>
+            <div class="item-input"><input type="text" class="albumName" disabled></div>
           </div>
           <div class="modal-item">
             <div class="item-header">Kind</div>
-            <div class="item-input"><select class="albumKind" name="" id="">
+            <div class="item-input"><select class="albumKind" name="" id="" disabled>
                 <?php foreach ($kinds as $k): ?>
                   <option value="<?= $k['maLoai'] ?>"><?= $k['tenLoai'] ?></option>
                 <?php endforeach ?>
@@ -92,24 +92,24 @@ $kinds = getKind();
           </div>
           <div class="modal-item">
             <div class="item-header">Artist name</div>
-            <div class="item-input"><input type="text" class="albumArtist" value="buibuibaobao@gmail.com"></div>
+            <div class="item-input"><input type="text" class="albumArtist" disabled></div>
           </div>
           <div class="modal-item">
             <div class="item-header">Quanitity</div>
-            <div class="item-input"><input type="text" class="albumQuantity" value="Ho Chi Minh City"></div>
+            <div class="item-input"><input type="text" class="albumQuanitity" disabled></div>
           </div>
           <div class="modal-item">
             <div class="item-header">Price</div>
-            <div class="item-input"><input type="text" class="albumPrice" value="0900000000"></div>
+            <div class="item-input"><input type="text" class="albumPrice" disabled></div>
           </div>
 
           <div class="modal-item">
             <div class="item-header">Image</div>
-            <div class="item-input"><input type="text" class="albumImg" value="baohongbui313"></div>
+            <div class="item-input"><input type="text" class="albumImg" disabled></div>
           </div>
           <div class="modal-item">
             <div class="item-header">Describe</div>
-            <div class="item-input"><textarea cols="30" class="albumDescribe" rows="6"></textarea></div>
+            <div class="item-input"><textarea cols="30" class="albumDescribe" rows="6" disabled></textarea></div>
           </div>
         </div>
       </div>
@@ -162,41 +162,41 @@ $kinds = getKind();
         <div class="modal-info">
           <div class="modal-item">
             <div class="item-header">Album id</div>
-            <div class="item-input"><input type="text" value="CUS001"></div>
+            <div class="item-input"><input type="text" class="albumID" disabled></div>
           </div>
           <div class="modal-item">
             <div class="item-header">Album name</div>
-            <div class="item-input"><input type="text" value="Bùi Hồng Bảo"></div>
+            <div class="item-input"><input type="text" class="albumName"></div>
           </div>
           <div class="modal-item">
             <div class="item-header">Kind</div>
-            <div class="item-input"><select name="" id="">
-                <option value="CUS">Customer</option>
-                <option value="EM">Employee</option>
-                <option value="AD">Admin</option>
+            <div class="item-input"><select class="albumKind" name="" id="">
+                <?php foreach ($kinds as $k): ?>
+                  <option value="<?= $k['maLoai'] ?>"><?= $k['tenLoai'] ?></option>
+                <?php endforeach ?>
               </select>
             </div>
           </div>
           <div class="modal-item">
             <div class="item-header">Artist name</div>
-            <div class="item-input"><input type="text" value="buibuibaobao@gmail.com"></div>
+            <div class="item-input"><input type="text" class="albumArtist"></div>
           </div>
           <div class="modal-item">
             <div class="item-header">Quanitity</div>
-            <div class="item-input"><input type="text" value="Ho Chi Minh City"></div>
+            <div class="item-input"><input type="text" class="albumQuanitity"></div>
           </div>
           <div class="modal-item">
             <div class="item-header">Price</div>
-            <div class="item-input"><input type="text" value="0900000000"></div>
+            <div class="item-input"><input type="text" class="albumPrice"></div>
           </div>
 
           <div class="modal-item">
             <div class="item-header">Image</div>
-            <div class="item-input"><input type="text" value="baohongbui313"></div>
+            <div class="item-input"><input type="text" class="albumImg"></div>
           </div>
           <div class="modal-item">
             <div class="item-header">Describe</div>
-            <div class="item-input"><textarea cols="30" rows="6"></textarea></div>
+            <div class="item-input"><textarea class="albumDescribe" cols="30" rows="6"></textarea></div>
           </div>
         </div>
       </div>
@@ -231,176 +231,6 @@ $kinds = getKind();
             <div class="info-placeholder">Save</div>
           </div>
           <div class="back-button" onclick="closeEditalbum()">
-            <div class="icon-placeholder"><i class="fa-solid fa-xmark"></i></div>
-            <div class="info-placeholder">Cancel</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal-placeholder" id="new-album">
-    <div class="modal-box">
-      <div class="modal-header">
-        <h1><i class="fa-regular fa-album-circle-plus"></i> New album</h1>
-      </div>
-      <div class="modal-left ">
-        <div class="modal-info">
-          <div class="modal-item">
-            <div class="item-header">Album id</div>
-            <div class="item-input"><input type="text" value="CUS001"></div>
-          </div>
-          <div class="modal-item">
-            <div class="item-header">Album name</div>
-            <div class="item-input"><input type="text" value="Bùi Hồng Bảo"></div>
-          </div>
-          <div class="modal-item">
-            <div class="item-header">Kind</div>
-            <div class="item-input"><select name="" id="">
-                <option value="CUS">Customer</option>
-                <option value="EM">Employee</option>
-                <option value="AD">Admin</option>
-              </select>
-            </div>
-          </div>
-          <div class="modal-item">
-            <div class="item-header">Artist name</div>
-            <div class="item-input"><input type="text" value="buibuibaobao@gmail.com"></div>
-          </div>
-          <div class="modal-item">
-            <div class="item-header">Quanitity</div>
-            <div class="item-input"><input type="text" value="Ho Chi Minh City"></div>
-          </div>
-          <div class="modal-item">
-            <div class="item-header">Price</div>
-            <div class="item-input"><input type="text" value="0900000000"></div>
-          </div>
-
-          <div class="modal-item">
-            <div class="item-header">Image</div>
-            <div class="item-input"><input type="text" value="baohongbui313"></div>
-          </div>
-          <div class="modal-item">
-            <div class="item-header">Describe</div>
-            <div class="item-input"><textarea cols="30" rows="6"></textarea></div>
-          </div>
-        </div>
-      </div>
-      <div class="modal-right ">
-        <div class="title-list">
-          <div class="title-placeholder">
-            <div class="title" style="padding-right: 10px;">No.</div>
-            <div class="title" style="padding-right: 15px;">Song ID</div>
-            <div class="title" style="padding-right: 15px;">Song name</div>
-            <div class="title" style="padding-right: 10px;">Song file</div>
-          </div>
-        </div>
-        <div class="list">
-          <div class="placeholder">
-            <div class="info">
-              <div class="item">01</div>
-              <div class="item">CUS001</div>
-              <div class="item">Bùi Hồng Bảo</div>
-              <div class="item">.mp3</div>
-              <div class="item"><i class="fa-solid fa-xmark-large fa-sm" style="color: #f2623e;"></i></div>
-            </div>
-          </div>
-          <div class="add-button"><i class="fa-solid fa-plus-large"></i></div>
-        </div>
-      </div>
-      <div class="modal-button">
-        <div class="button-layout"></div>
-        <div class="button-layout">
-          <div></div>
-          <div class="edit-button">
-            <div class="icon-placeholder"><i class="fa-solid fa-folder-arrow-down"></i></div>
-            <div class="info-placeholder">Add</div>
-          </div>
-          <div class="back-button" onclick="closeNewalbum()">
-            <div class="icon-placeholder"><i class="fa-solid fa-xmark"></i></div>
-            <div class="info-placeholder">Cancel</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="modal-placeholder" id="restock-album">
-    <div class="modal-box">
-      <div class="modal-header ">
-        <h1><i class="fa-solid fa-rotate-left"></i> Restock album</h1>
-      </div>
-      <div class="modal-left ">
-        <div class="modal-info">
-          <div class="modal-item">
-            <div class="item-header">Album id</div>
-            <div class="item-input"><input type="text" value="CUS001"></div>
-          </div>
-          <div class="modal-item">
-            <div class="item-header">Album name</div>
-            <div class="item-input"><input type="text" value="Bùi Hồng Bảo"></div>
-          </div>
-          <div class="modal-item">
-            <div class="item-header">Kind</div>
-            <div class="item-input"><select name="" id="">
-                <option value="CUS">Customer</option>
-                <option value="EM">Employee</option>
-                <option value="AD">Admin</option>
-              </select>
-            </div>
-          </div>
-          <div class="modal-item">
-            <div class="item-header">Artist name</div>
-            <div class="item-input"><input type="text" value="buibuibaobao@gmail.com"></div>
-          </div>
-          <div class="modal-item">
-            <div class="item-header">Quanitity</div>
-            <div class="item-input"><input type="text" value="Ho Chi Minh City"></div>
-          </div>
-          <div class="modal-item">
-            <div class="item-header">Price</div>
-            <div class="item-input"><input type="text" value="0900000000"></div>
-          </div>
-
-          <div class="modal-item">
-            <div class="item-header">Image</div>
-            <div class="item-input"><input type="text" value="baohongbui313"></div>
-          </div>
-          <div class="modal-item">
-            <div class="item-header">Describe</div>
-            <div class="item-input"><textarea cols="30" rows="6"></textarea></div>
-          </div>
-        </div>
-      </div>
-      <div class="modal-right ">
-        <div class="title-list">
-          <div class="title-placeholder">
-            <div class="title" style="padding-right: 10px;">No.</div>
-            <div class="title" style="padding-right: 15px;">Song ID</div>
-            <div class="title" style="padding-right: 15px;">Song name</div>
-            <div class="title" style="padding-right: 10px;">Song file</div>
-          </div>
-        </div>
-        <div class="list">
-          <div class="placeholder">
-            <div class="info">
-              <div class="item">01</div>
-              <div class="item">CUS001</div>
-              <div class="item">Bùi Hồng Bảo</div>
-              <div class="item">.mp3</div>
-              <div class="item"><i class="fa-solid fa-xmark-large fa-sm" style="color: #f2623e;"></i></div>
-            </div>
-          </div>
-          <div class="add-button"><i class="fa-solid fa-plus-large"></i></div>
-        </div>
-      </div>
-      <div class="modal-button">
-        <div class="button-layout"></div>
-        <div class="button-layout">
-          <div></div>
-          <div class="edit-button">
-            <div class="icon-placeholder"><i class="fa-solid fa-folder-arrow-down"></i></div>
-            <div class="info-placeholder">Add</div>
-          </div>
-          <div class="back-button" onclick="closeRestockalbum()">
             <div class="icon-placeholder"><i class="fa-solid fa-xmark"></i></div>
             <div class="info-placeholder">Cancel</div>
           </div>
