ffba129e1212b8d0a299802fd2d2be9e63b3417b
PhuQuangKoiNe <tnpquang@gmail.com>
2023-04-22 19:35:28 +0700

    edit roleManager

--- views/pages/admin/roleManager.php
+++ views/pages/admin/roleManager.php
@@ -20,10 +20,165 @@
                 <input type="checkbox">
             </div>
         </div>
+        <div class="role-box">
+            <div class="role-header">Product management</div>
+            <div class="role-item">Access</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+            <div class="role-item">Publish</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+            <div class="role-item">Delete</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+            <div class="role-item">Edit</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+        </div><div class="role-box">
+            <div class="role-header">Product management</div>
+            <div class="role-item">Access</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+            <div class="role-item">Publish</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+            <div class="role-item">Delete</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+            <div class="role-item">Edit</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+        </div><div class="role-box">
+            <div class="role-header">Product management</div>
+            <div class="role-item">Access</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+            <div class="role-item">Publish</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+            <div class="role-item">Delete</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+            <div class="role-item">Edit</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+        </div><div class="role-box">
+            <div class="role-header">Product management</div>
+            <div class="role-item">Access</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+            <div class="role-item">Publish</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+            <div class="role-item">Delete</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+            <div class="role-item">Edit</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+        </div><div class="role-box">
+            <div class="role-header">Product management</div>
+            <div class="role-item">Access</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+            <div class="role-item">Publish</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+            <div class="role-item">Delete</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+            <div class="role-item">Edit</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+        </div><div class="role-box">
+            <div class="role-header">Product management</div>
+            <div class="role-item">Access</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+            <div class="role-item">Publish</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+            <div class="role-item">Delete</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+            <div class="role-item">Edit</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+        </div><div class="role-box">
+            <div class="role-header">Product management</div>
+            <div class="role-item">Access</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+            <div class="role-item">Publish</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+            <div class="role-item">Delete</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+            <div class="role-item">Edit</div>
+            <div class="checkbox-placeholder">
+                <input type="checkbox">
+            </div>
+        </div>
     </div>
     <h1>Permission</h1>
     <div class="permission-placeholder">
-        <div class="title-list">
+        <div class="title-placeholder">
+            <div class="title">No.</div>
+            <div class="title">Role id</div>
+            <div class="title">Role name</div>
+            <div class="title">Description</div>
+            <div class="title"></div>
+
+        </div>
+        <div class="list-placeholder">
+                <div class="role-information">
+                    <div class="item">01</div>
+                    <div class="item">EMP1</div>
+                    <div class="item"><input type="text" value="Employee 1"> </div>
+                    <div class="item"> <input type="text"  value="Loremakjshdksajdhksajhdkjsahdsahdkahdkjashd;hsa;dhakdhafsshkkkkkkkkkkkkkkkkkkkkkkkkkkshfkhsdkjhfkshfkjshkfhskdjfhksjdfsdfsdfsds"> </div>
+                    <div class="item"><i class="fa-regular fa-circle-info"></i></div>
+                </div>
+                <div class="role-information">
+                    <div class="item">01</div>
+                    <div class="item">EMP1</div>
+                    <div class="item">Employee 1</div>
+                    <div class="item">Lor emakjs hdksa jdhks ajhdkj sahd sahdk ahdkjashd Lor emakjs hdksa jdhks ajhdkj sahd sahdk ahdkjashd Lor emakjs hdksa jdhks ajhdkj sahd sahdk ahdkjashdLor emakjs hdksa jdhks ajhdkj sahd sahdk ahdkjashd Lor emakjs hdksa jdhks ajhdkj sahd sahdk ahdkjashd</div>
+                    <div class="item"><i class="fa-regular fa-circle-info"></i></div>
+                </div><div class="role-information">
+                    <div class="item">01</div>
+                    <div class="item">EMP1</div> 
+                    <div class="item">Employee 1</div>
+                    <div class="item">Lorem</div>
+                    <div class="item"><i class="fa-regular fa-circle-info"></i></div>
+                </div>
         </div>
     </div>
 </div>
