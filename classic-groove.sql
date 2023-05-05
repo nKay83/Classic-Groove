caf088c0e8f4850c1ccc2abb828cfcace0ec5841
BuiBao3103 <hongbao2003@gmail.com>
2023-05-05 13:19:47 +0700

    database

--- classic-groove.sql
+++ classic-groove.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 21, 2023 at 08:30 AM
+-- Generation Time: May 05, 2023 at 08:18 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
@@ -44,14 +44,14 @@ CREATE TABLE `album` (
 --
 
 INSERT INTO `album` (`maAlbum`, `tenAlbum`, `gia`, `moTa`, `hinh`, `tacGia`, `TrangThai`, `soLuong`, `theLoai`) VALUES
-(1, 'Evo Sessions', 100, 'Evosound proudly presents Chlara\\\'s evo sessions with 11 tracks on the LP of her favorite classic songs, mainly with acoustic accompaniment. These famous classic songs can resonate throughout different generations from early 20\'s to 21st century like \"Love Me Tender\" or \"Stay With Me\". Chlara is a British-born singer / songwriter recognised as an acoustic songbird, learned to sing and play guitar when she was a young teenager. \"evo sessions\" is her third album release in 4 years. Chlara is not only young and pretty, she possess a lovely sweet voice, and when you hear her sing with a high dynamic range, you can totally feel the passion and energy she puts into her songs. Included in this album is a song she wrote herself called \"Bliss\". The audio of evo sessions was recorded during a live video shoot. The idea of evo sessions started since 2017 summer, the live music video was shot in studio and because of the fabulous sound quality, evosound decided to release as a full album.', 'EvoSessions-Chlara', 'Chlara', 1, 4, 2),
-(2, '#acousticNOW', 200, ' AcousticNow# is the eagerly awaitied fourth album by the 26-year-old Filipino singing sensation Chlara, who will captivate her listeners with a series of acoustic cover versions that include her singles \'The Nights,\' \'Say You Won\'t Let Go,\' and \'ILYSB,\' which have all received over 20 million plays on Spotify. Chlara takes a varied selection of songs - from hits by Ed Sheeran and Aviici to Justin Bieber and Tears For Fears - and reimagines them in her own unique way. Noted for her alluring honey-toned voice and deft acoustic guitar accompaniment, Chlara, who has been described as an \"acoustic songbird\" is flying high at the moment. With three albums and several EPs already under her belt for the Evosound label, she\'s about to unleash her fourth, acousticNow#. The England-born, Philippines-raised singer/song-writer\'s popularity can be measured by that fact that three songs of her songs have generated over 20 million plays each on Spotify.', 'AcousticNow-Chlara', 'Chlara', 1, 3, 2),
-(3, 'It Serves You Right To Suffer', 200, '180-gram 45 RPM double LP Mastered at AcousTech Mastering Plated and pressed at Quality Record PressingsIt\'s back in stock! Newly repressed at 45 RPM by Quality Record Pressings. This recording was originally mastered at the former AcousTech Mastering facility and the sound is fantastic.John Lee Hooker himself did not know his exact date of birth. If he hadn\'t died at around the age of 80, this ageless musician would still be easily pulling the next generation to his gigs. Hooker remains a phenomenon, a mysterious figure of black rhythm and blues, a charismatic king who reigns supreme in rock \'n\' roll\'s Hall of Fame. John W. Peters described his music as a synthesis of scorching emotional ardour, unrelenting rhythmic intensity, and original poetry of a highly personal character. Anyone hearing him for the first time may well be startled at the unfiltered passion and power of his music.', 'ItServeYouRightToSuffer-JohnLeeHooker', 'John Lee Hooker', 1, 6, 1),
-(4, 'Love For Sale', 100, 'Celebrating 10 years since they first recorded together, Tony Bennett & Lady Gaga return for another collaboration featuring the best of the Cole Porter Songbook. It captures the creative and personal relationship of these two world-famous artists. Tony, who turned 95 in 2021, has spent over 7 decades dedicated to performing the Great American Songbook. They are accompanied by the Brian Newman Quintet with arrangements by Marion Evans and Jorge Callandrelli. 180-gram vinyl.', 'LoveForSale-TonyBennett_LadyGaga', 'Tony Bennett & Lady Gaga', 1, 5, 6),
-(5, 'Dawn FM', 120, 'he Weeknd deemed his 2022 album, Dawn FM, a \"sonic experience\" showcasing a unique cast of features from Tyler, the Creator, Lil Wayne, Quincy Jones, Oneohtrix Point Never, and Jim Carrey. (XO Records/Republic)', 'DawnFM-TheWeeknd', 'TheWeeknd', 1, 3, 5),
-(6, 'Fearless (Taylor’s Version) Gold', 250, '\"Fearless was an album full of magic and curiosity, the bliss and devastation of youth. It was the diary of the adventures and explorations of a teenage girl who was learning tiny lessons with every new crack in the facade of the fairytale ending she\'d been shown in the movies. I\'m thrilled to tell you that my new version of Fearless is done and will be with you soon. It\'s called Fearless (Taylor\'s Version) and it includes 26 songs.\" - Taylor Swift. Includes 6 unreleased tracks. Gold 3 LP.', 'Fearless-TaylorSwift', 'Taylor Swift', 1, 2, 4),
-(7, 'Beleive', 100, 'Internationally renowned Italian tenor Andrea Bocelli releases breathtaking new album, Believe, celebrating the power of music to soothe the soul. It follows his record-breaking `Music for Hope\' performance at Easter from Milan\'s historic Duomo cathedral. Features classic favorites, a previously unreleased track from late Italian composer Ennio Morricone, Gratia Plena (from acclaimed film Fatima), duets w/ Alison Krauss & Cecilia Bartoli and interpretations of Ave Maria and Cohen\'s Hallelujah.', 'Believe-AndreaBocelli', 'Andrea Bocelli', 1, 7, 3),
-(8, 'Jordi', 100, 'The eagerly awaited new album from Maroon 5, JORDI, is the band\'s first since the critically acclaimed Red Pill Blues. The album will include the recently released hit single \"Beautiful Mistakes ft. Megan Thee Stallion\" as well as fan favorites \"Memories\" and \"Nobody\'s Love.\" Vinyl LP pressing. 2021 album.', 'Jordi-Maroon5', 'Maroon 5', 1, 4, 7);
+(1, 'Evo Sessionss', 100, 'Evosound proudly presents Chlara\'s evo sessions with 11 tracks on the LP of her favorite classic songs, mainly with acoustic accompaniment. These famous classic songs can resonate throughout different generations from early 20\'s to 21st century like \"Love Me Tender\" or \"Stay With Me\". Chlara is a British-born singer / songwriter recognised as an acoustic songbird, learned to sing and play guitar when she was a young teenager. \"evo sessions\" is her third album release in 4 years. Chlara is not only young and pretty, she possess a lovely sweet voice, and when you hear her sing with a high dynamic range, you can totally feel the passion and energy she puts into her songs. Included in this album is a song she wrote herself called \"Bliss\". The audio of evo sessions was recorded during a live video shoot. The idea of evo sessions started since 2017 summer, the live music video was shot in studio and because of the fabulous sound quality, evosound decided to release as a full album.', 'EvoSessions-Chlara.jpg', 'Chlara', 1, 4, 2),
+(2, '#acousticNOW', 200, ' AcousticNow# is the eagerly awaitied fourth album by the 26-year-old Filipino singing sensation Chlara, who will captivate her listeners with a series of acoustic cover versions that include her singles \'The Nights,\' \'Say You Won\'t Let Go,\' and \'ILYSB,\' which have all received over 20 million plays on Spotify. Chlara takes a varied selection of songs - from hits by Ed Sheeran and Aviici to Justin Bieber and Tears For Fears - and reimagines them in her own unique way. Noted for her alluring honey-toned voice and deft acoustic guitar accompaniment, Chlara, who has been described as an \"acoustic songbird\" is flying high at the moment. With three albums and several EPs already under her belt for the Evosound label, she\'s about to unleash her fourth, acousticNow#. The England-born, Philippines-raised singer/song-writer\'s popularity can be measured by that fact that three songs of her songs have generated over 20 million plays each on Spotify.', 'AcousticNow-Chlara.jpg', 'Chlara', 1, 3, 2),
+(3, 'It Serves You Right To Suffer', 200, '180-gram 45 RPM double LP Mastered at AcousTech Mastering Plated and pressed at Quality Record PressingsIt\'s back in stock! Newly repressed at 45 RPM by Quality Record Pressings. This recording was originally mastered at the former AcousTech Mastering facility and the sound is fantastic.John Lee Hooker himself did not know his exact date of birth. If he hadn\'t died at around the age of 80, this ageless musician would still be easily pulling the next generation to his gigs. Hooker remains a phenomenon, a mysterious figure of black rhythm and blues, a charismatic king who reigns supreme in rock \'n\' roll\'s Hall of Fame. John W. Peters described his music as a synthesis of scorching emotional ardour, unrelenting rhythmic intensity, and original poetry of a highly personal character. Anyone hearing him for the first time may well be startled at the unfiltered passion and power of his music.', 'ItServeYouRightToSuffer-JohnLeeHooker.jpg', 'John Lee Hooker', 1, 6, 1),
+(4, 'Love For Sale', 100, 'Celebrating 10 years since they first recorded together, Tony Bennett & Lady Gaga return for another collaboration featuring the best of the Cole Porter Songbook. It captures the creative and personal relationship of these two world-famous artists. Tony, who turned 95 in 2021, has spent over 7 decades dedicated to performing the Great American Songbook. They are accompanied by the Brian Newman Quintet with arrangements by Marion Evans and Jorge Callandrelli. 180-gram vinyl.', 'LoveForSale-TonyBennett_LadyGaga.jpg', 'Tony Bennett & Lady Gaga', 1, 5, 6),
+(5, 'Dawn FM', 120, 'he Weeknd deemed his 2022 album, Dawn FM, a \"sonic experience\" showcasing a unique cast of features from Tyler, the Creator, Lil Wayne, Quincy Jones, Oneohtrix Point Never, and Jim Carrey. (XO Records/Republic)', 'DawnFM-TheWeeknd.jpg', 'TheWeeknd', 1, 3, 5),
+(6, 'Fearless (Taylor’s Version) Gold', 250, '\"Fearless was an album full of magic and curiosity, the bliss and devastation of youth. It was the diary of the adventures and explorations of a teenage girl who was learning tiny lessons with every new crack in the facade of the fairytale ending she\'d been shown in the movies. I\'m thrilled to tell you that my new version of Fearless is done and will be with you soon. It\'s called Fearless (Taylor\'s Version) and it includes 26 songs.\" - Taylor Swift. Includes 6 unreleased tracks. Gold 3 LP.', 'Fearless-TaylorSwift.jpg', 'Taylor Swift', 1, 2, 4),
+(7, 'Beleive', 100, 'Internationally renowned Italian tenor Andrea Bocelli releases breathtaking new album, Believe, celebrating the power of music to soothe the soul. It follows his record-breaking `Music for Hope\' performance at Easter from Milan\'s historic Duomo cathedral. Features classic favorites, a previously unreleased track from late Italian composer Ennio Morricone, Gratia Plena (from acclaimed film Fatima), duets w/ Alison Krauss & Cecilia Bartoli and interpretations of Ave Maria and Cohen\'s Hallelujah.', 'Believe-AndreaBocelli.jpg', 'Andrea Bocelli', 1, 7, 3),
+(8, 'Jordi', 100, 'The eagerly awaited new album from Maroon 5, JORDI, is the band\'s first since the critically acclaimed Red Pill Blues. The album will include the recently released hit single \"Beautiful Mistakes ft. Megan Thee Stallion\" as well as fan favorites \"Memories\" and \"Nobody\'s Love.\" Vinyl LP pressing. 2021 album.', 'Jordi-Maroon5.jpg', 'Maroon 5', 1, 4, 7);
 
 -- --------------------------------------------------------
 
@@ -85,7 +85,11 @@ INSERT INTO `baihat` (`maBaiHat`, `tenBaiHat`, `linkFile`) VALUES
 (13, 'Believe', 'Believe-Hallelujah\r\n'),
 (14, 'Ave Maria', 'Believe-AveMaria'),
 (15, 'Beautiful Mistakes', 'Jordi-BeautifulMistakes\r\n'),
-(16, 'Lost', 'Jordi-Lost');
+(16, 'Lost', 'Jordi-Lost'),
+(17, 'Stay With Me', 'Chlara  Stay With Me evo sessions Live'),
+(18, 'True Colours', 'Chlara  True Colours evo sessions Live'),
+(19, 'aaaa', 'Facebook'),
+(20, '1234', 'Facebook');
 
 -- --------------------------------------------------------
 
@@ -118,7 +122,9 @@ INSERT INTO `baihat_album` (`BaiHat_maBaiHat`, `Album_maAlbum`) VALUES
 (13, 7),
 (14, 7),
 (15, 8),
-(16, 8);
+(16, 8),
+(17, 1),
+(18, 1);
 
 -- --------------------------------------------------------
 
@@ -154,8 +160,7 @@ CREATE TABLE `chitietphieunhap` (
   `album` int(11) NOT NULL,
   `phieuNhap` int(11) NOT NULL,
   `gia` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `SoLuong` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `NCC` int(11) DEFAULT NULL
+  `SoLuong` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -191,10 +196,10 @@ CREATE TABLE `hoadon` (
 --
 
 INSERT INTO `hoadon` (`maHoaDon`, `tongTien`, `thoiGianDat`, `trangThai`, `khachHang`, `khuyenMai`, `diaChiGiaoHang`) VALUES
-(2, 315, '2023-04-12', 'Delivered', 'Lavied', NULL, 'B4/24G'),
+(2, 315, '2023-04-12', 'Cancel', 'Lavied', NULL, 'B4/24G'),
 (3, 615, '2023-04-15', 'Cancel', 'Lavied', NULL, 'mhfjfjfj'),
 (5, 1515, '2023-04-18', 'Pending', 'thuannguyen', NULL, 'Bình Chánh'),
-(6, 115, '2023-04-18', 'Cancel', 'lavied', NULL, 'B4/24G Trần Đại Nghĩa, Tân Kiên, Bình Chánh'),
+(6, 115, '2023-04-18', 'Pending', 'lavied', NULL, 'B4/24G Trần Đại Nghĩa, Tân Kiên, Bình Chánh'),
 (7, 930, '2023-04-21', 'Pending', 'lavied', NULL, 'B4/24G Trần Đại Nghĩa, Tân Kiên, Bình Chánh');
 
 -- --------------------------------------------------------
@@ -269,8 +274,20 @@ CREATE TABLE `nhacungcap` (
   `diaChi` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `SDT` varchar(12) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `email` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `TrangThai` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL
+  `TrangThai` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `nhacungcap`
+--
+
+INSERT INTO `nhacungcap` (`maNCC`, `tenNCC`, `diaChi`, `SDT`, `email`, `TrangThai`) VALUES
+(0, 'Công ty TNHH XYZ', '456 Đường D, Quận E, TP F', '0123456789', 'xyz@xyz.com', 1),
+(1, 'Công ty TNHH ABC', '123 Đường A, Quận B, TP C', '0987654321', 'abc@abc.com', 1),
+(2, 'Công ty TNHH XYZ', '456 Đường D, Quận E, TP F', '0123456789', 'xyz@xyz.com', 1),
+(3, 'Công ty TNHH LM', '789 Đường G, Quận H, TP I', '0987123456', 'lmh@lmn.com', 1),
+(4, ' Công ty TNHH PQR', '321 Đường J, Quận K, TP L', '0123789456', 'pqr@pqr.com', 1),
+(5, 'Công ty TNHH UVW', '654 Đường M, Quận N, TP O', '0912345678', 'uvw@uvw.com', 1);
 
 -- --------------------------------------------------------
 
@@ -282,8 +299,17 @@ CREATE TABLE `phieunhap` (
   `maPhieuNhap` int(11) NOT NULL,
   `ngayNhap` date DEFAULT NULL,
   `nguoiNhap` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
-  `TongGia` int(11) DEFAULT NULL
+  `TongGia` int(11) DEFAULT NULL,
+  `NCC` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `phieunhap`
+--
+
+INSERT INTO `phieunhap` (`maPhieuNhap`, `ngayNhap`, `nguoiNhap`, `TongGia`, `NCC`) VALUES
+(1, '2023-05-10', 'superadmin', 1000, 1),
+(2, '2023-05-12', 'superadmin', 10000, 4);
 
 -- --------------------------------------------------------
 
@@ -486,8 +512,7 @@ ALTER TABLE `chitiethoadon`
 --
 ALTER TABLE `chitietphieunhap`
   ADD PRIMARY KEY (`album`,`phieuNhap`),
-  ADD KEY `phieuNhap` (`phieuNhap`),
-  ADD KEY `NCC` (`NCC`);
+  ADD KEY `phieuNhap` (`phieuNhap`);
 
 --
 -- Indexes for table `giohang`
@@ -534,7 +559,8 @@ ALTER TABLE `nhacungcap`
 --
 ALTER TABLE `phieunhap`
   ADD PRIMARY KEY (`maPhieuNhap`),
-  ADD KEY `nguoiNhap` (`nguoiNhap`);
+  ADD KEY `nguoiNhap` (`nguoiNhap`),
+  ADD KEY `NCC` (`NCC`);
 
 --
 -- Indexes for table `quyen`
@@ -580,6 +606,12 @@ ALTER TABLE `yeuthich`
 --
 
 --
+-- AUTO_INCREMENT for table `baihat`
+--
+ALTER TABLE `baihat`
+  MODIFY `maBaiHat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
 -- AUTO_INCREMENT for table `hoadon`
 --
 ALTER TABLE `hoadon`
@@ -599,8 +631,8 @@ ALTER TABLE `album`
 -- Constraints for table `baihat_album`
 --
 ALTER TABLE `baihat_album`
-  ADD CONSTRAINT `baihat_album_ibfk_1` FOREIGN KEY (`BaiHat_maBaiHat`) REFERENCES `baihat` (`maBaiHat`),
-  ADD CONSTRAINT `baihat_album_ibfk_2` FOREIGN KEY (`Album_maAlbum`) REFERENCES `album` (`maAlbum`);
+  ADD CONSTRAINT `baihat_album_ibfk_2` FOREIGN KEY (`Album_maAlbum`) REFERENCES `album` (`maAlbum`),
+  ADD CONSTRAINT `baihat_album_ibfk_3` FOREIGN KEY (`BaiHat_maBaiHat`) REFERENCES `baihat` (`maBaiHat`);
 
 --
 -- Constraints for table `chitiethoadon`
@@ -614,8 +646,7 @@ ALTER TABLE `chitiethoadon`
 --
 ALTER TABLE `chitietphieunhap`
   ADD CONSTRAINT `chitietphieunhap_ibfk_1` FOREIGN KEY (`album`) REFERENCES `album` (`maAlbum`),
-  ADD CONSTRAINT `chitietphieunhap_ibfk_2` FOREIGN KEY (`phieuNhap`) REFERENCES `phieunhap` (`maPhieuNhap`),
-  ADD CONSTRAINT `chitietphieunhap_ibfk_3` FOREIGN KEY (`NCC`) REFERENCES `nhacungcap` (`maNCC`);
+  ADD CONSTRAINT `chitietphieunhap_ibfk_2` FOREIGN KEY (`phieuNhap`) REFERENCES `phieunhap` (`maPhieuNhap`);
 
 --
 -- Constraints for table `giohang`
@@ -641,7 +672,8 @@ ALTER TABLE `nguoidung`
 -- Constraints for table `phieunhap`
 --
 ALTER TABLE `phieunhap`
-  ADD CONSTRAINT `phieunhap_ibfk_1` FOREIGN KEY (`nguoiNhap`) REFERENCES `nguoidung` (`maNguoiDung`);
+  ADD CONSTRAINT `phieunhap_ibfk_1` FOREIGN KEY (`nguoiNhap`) REFERENCES `nguoidung` (`maNguoiDung`),
+  ADD CONSTRAINT `phieunhap_ibfk_2` FOREIGN KEY (`NCC`) REFERENCES `nhacungcap` (`maNCC`);
 
 --
 -- Constraints for table `taikhoan`
