c7cb32fa6898c193f17ccefd1efa6e93dc292504
BuiBao3103 <hongbao2003@gmail.com>
2023-05-08 19:40:37 +0700

    front end role

---
+++ views/pages/admin/detailRole.php
@@ -0,0 +1,131 @@
+<h1>Role descriptions</h1>
+<div class="info-role">
+    <div class="role-header">Role infomation</div>
+    <div class="row">
+        <label for="">ID:</label>
+        <Span>1</Span>
+        <!-- <input type="text" value="1"> -->
+    </div>
+    <div class="row">
+        <label for="">Name:</label>
+        <input type="text" value="1">
+    </div>
+    <div class="row">
+        <label for="">Des:</label>
+        <textarea name="" id="" cols="30" rows="5"></textarea>
+    </div>
+</div>
+<div class="role-placeholder">
+    <div class="role-box">
+        <div class="role-header">Product management</div>
+        <div class="role-item">Access</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+        <div class="role-item">Delete</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+        <div class="role-item">Edit</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+    </div>
+    <div class="role-box">
+        <div class="role-header">Supply management</div>
+        <div class="role-item">Access</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+        <div class="role-item">Add</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+    </div>
+    <div class="role-box">
+        <div class="role-header">Account management</div>
+        <div class="role-item">Access</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+        <div class="role-item">Add</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+        <div class="role-item">Edit</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+        <div class="role-item">Block</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+    </div>
+    <div class="role-box">
+        <div class="role-header">Order management</div>
+        <div class="role-item">Access</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+        <div class="role-item">Edit</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+    </div>
+    <div class="role-box">
+        <div class="role-header">Structure management</div>
+        <div class="role-item">Access</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+        <div class="role-item">Edit</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+    </div>
+    <div class="role-box">
+        <div class="role-header">Role management</div>
+        <div class="role-item">Access</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+        <div class="role-item">Add</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+        <div class="role-item">Edit</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+        <div class="role-item">Delete</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+    </div>
+    <div class="role-box">
+        <div class="role-header">Statistic management</div>
+        <div class="role-item">Access</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+    </div>
+    <div class="role-box">
+        <div class="role-header">Distributor management</div>
+        <div class="role-item">Access</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+        <div class="role-item">Add</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+        <div class="role-item">Edit</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+        <div class="role-item">Delete</div>
+        <div class="checkbox-placeholder">
+            <input type="checkbox">
+        </div>
+    </div>
+</div>
