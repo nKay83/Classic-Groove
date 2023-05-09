920d1a4246994854ddb23af682508b0fd98acb1e
BuiBao3103 <hongbao2003@gmail.com>
2023-05-09 22:08:09 +0700

    load slide show and css

--- views/pages/admin/structureManager.php
+++ views/pages/admin/structureManager.php
@@ -1,3 +1,10 @@
+<?php
+require("../../../util/dataProvider.php");
+$dp = new DataProvider();
+$slideShow = getAllSlideShow();
+print_r($slideShow)
+    ?>
+
 <div id="structure">
     <h1><i class="fa-solid fa-puzzle"></i> Structure management</h1>
     <div class="manageSilder">
@@ -32,18 +39,42 @@
                 </div>
             </div>
             <div class="list">
-                <div class="silde-placeholder">
-                    <div class="item">1</div>
-                    <div class="item">
-                        <div class="img-placeholder">
-                            <img src="" alt="">
+                <?php for ($i = 0; $i < count($slideShow); $i++): ?>
+                    <div class="silde-placeholder">
+                        <div class="item">
+                            <?= sprintf("%02d", $i + 1) ?>
                         </div>
+                        <div class="item">
+                            <div class="img-placeholder">
+                                <img src="data/slideShow/<?= $slideShow[$i]['linkHinh'] ?>" alt="imgSlideShow">
+                            </div>
+                        </div>
+                        <div class="item">
+                            <?= $slideShow[$i]['tenHinh'] ?>
+                        </div>
+                        <div class="item">
+                            <?= $slideShow[$i]['linkTo'] ?>
+                        </div>
+                        <div class="item"><i class="fa-regular fa-circle-info"></i></div>
                     </div>
-                    <div class="item">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea dolore unde nemo libero ab eaque at voluptatu</div>
-                    <div class="item">1</div>
-                    <div class="item"><i class="fa-regular fa-circle-info"></i></div>
-                </div>
+                <?php endfor ?>
             </div>
         </div>
     </div>
 </div>
+
+<?php
+function getAllSlideShow()
+{
+    global $dp;
+    $sql = "SELECT * FROM slideshow";
+    $result = $dp->excuteQuery($sql);
+    $slideShow = array();
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            array_push($slideShow, $row);
+        }
+    }
+    return $slideShow;
+}
+?>
